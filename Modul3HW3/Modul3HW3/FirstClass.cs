0430b1ec7504bbb60d8143f4dad3ccf90c4e2bbe
Yurii <leoonovn@gmail.com>
2021-08-04 16:36:27 +0300

    Забыл сделать первый коммит

---
+++ Modul3HW3/Modul3HW3/FirstClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW3
+{
+    public class FirstClass
+    {
+        public event Action<bool> Act;
+
+        public int Pow(int x, int y)
+        {
+            return x * y;
+        }
+
+        private void OnHandler(bool status)
+        {
+            Act?.Invoke(status);
+        }
+    }
+}
